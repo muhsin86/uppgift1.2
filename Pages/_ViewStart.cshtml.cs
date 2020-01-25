80e1462f056d1d74c00906b958665e43c3a197e2
Mohsen <muhsin86@hotmail.se>
2020-01-25 23:12:48 +0100

    Add project files.

---
+++ Pages/_ViewStart.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Uppgift1._2.Pages
+{
+    public class _ViewStartModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
